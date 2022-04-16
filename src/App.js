717f9c878b2c7faf7986d55b39dec2ca1d606c13
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-16 22:46:58 +0600

    website routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+// import logo from "./logo.svg";
+// import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import About from "./components/Pages/About/About";
+import Blog from "./components/Pages/Blog/Blog";
+import Home from "./components/Pages/Home/Home";
+import NotFound from "./components/Pages/NotFound/NotFound";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from tutor plus</h1>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blog">
+                Blog
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
