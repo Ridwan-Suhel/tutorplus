168fe1b1fa400c396197345171bb650e99ae8414
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-17 13:45:10 +0600

    login and signout header toggle added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 import CustomLink from "../CustomLink/CustomLink";
 import "./Header.css";
 const Header = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const handleSignOut = () => {
+    signOut(auth);
+  };
   return (
     <header>
       <Navbar expand="lg" className="py-4 my-2">
@@ -19,7 +26,13 @@ const Header = () => {
               <CustomLink to="/">Home</CustomLink>
               <CustomLink to="/about">About</CustomLink>
               <CustomLink to="/blog">Blog</CustomLink>
-              <CustomLink to="/login">Login</CustomLink>
+              {user ? (
+                <CustomLink to="/login" onClick={handleSignOut}>
+                  Logout
+                </CustomLink>
+              ) : (
+                <CustomLink to="/login">Login</CustomLink>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/components/Pages/Home/Login/Login.js
+++ src/components/Pages/Home/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link, Navigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import auth from "../../../../firebase.init";
 import "./Login.css";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
@@ -21,9 +21,10 @@ const Login = () => {
     signInWithEmailAndPassword(email, password);
   };
 
+  const navigate = useNavigate();
   useEffect(() => {
     if (user) {
-      Navigate("/");
+      navigate("/");
     }
   }, [user]);
 
@@ -37,12 +38,20 @@ const Login = () => {
               <Form onSubmit={formSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Control
+                    ref={emailRef}
+                    type="email"
+                    placeholder="Enter email"
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control
+                    ref={passwordRef}
+                    type="password"
+                    placeholder="Password"
+                  />
                 </Form.Group>
                 {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
                   <Form.Check type="checkbox" label="Check me out" />
