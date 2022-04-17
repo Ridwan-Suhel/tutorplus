4259e2ef3f31f6bf4eb99351e417102fb854cf99
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-17 12:59:24 +0600

    signup ui added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Blog from "./components/Pages/Blog/Blog";
 import Home from "./components/Pages/Home/Home";
 import Login from "./components/Pages/Home/Login/Login";
 import NotFound from "./components/Pages/NotFound/NotFound";
+import Signup from "./components/Pages/Signup/Signup";
 function App() {
   return (
     <div>
@@ -20,6 +21,7 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/blog" element={<Blog></Blog>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
         <Route path="/checkout" element={<CheckOut></CheckOut>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
--- src/components/Pages/Signup/Signup.js
+++ src/components/Pages/Signup/Signup.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./Login.css";
 
-const Login = () => {
+const Signup = () => {
   return (
     <section className="section">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-7 col-lg-6">
             <div className="form-wrapper my-5">
-              <p className="lead fw-bold">Please Login</p>
+              <p className="lead fw-bold">Please Signup</p>
               <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                  <Form.Label>Your name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter name" />
+                </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
                   <Form.Control type="email" placeholder="Enter email" />
@@ -21,14 +25,11 @@ const Login = () => {
                   <Form.Label>Password</Form.Label>
                   <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
-                {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group> */}
                 <p className="lead">
-                  Don't have an account? <Link to="/signup">Signup Here</Link>
+                  Already have an account? <Link to="/login">Signin Here</Link>
                 </p>
                 <Button variant="dark" className="px-5 mt-2 py-2" type="submit">
-                  Login
+                  Signup
                 </Button>
               </Form>
             </div>
@@ -39,4 +40,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
