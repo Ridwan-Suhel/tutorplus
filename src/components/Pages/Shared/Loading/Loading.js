aa56101a61e62f16ccbdc2a338567fe132ca8f61
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-17 16:06:22 +0600

    loading added for requireAuth

---
+++ src/components/Pages/Shared/Loading/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Spinner } from "react-bootstrap";
+
+const Loading = () => {
+  return (
+    <div
+      className="container d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  );
+};
+
+export default Loading;
