9f1d3f8d46f8de09daeaa275bd82568932bd099d
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-17 20:58:48 +0600

    Not found page completed

--- src/components/Pages/NotFound/NotFound.js
+++ src/components/Pages/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import four0four from "../../../images/giphy.gif";
+import "./NotFound.css";
 const NotFound = () => {
   return (
-    <div>
-      <h1>Hello from Not found</h1>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-5 notFound mt-5">
+          <p className="lead text-center display-6 mb-3">
+            Oops! Page Not Found.
+          </p>
+          <img
+            className="img-fluid w-100 mb-5"
+            src={four0four}
+            alt="not found"
+          />
+        </div>
+      </div>
     </div>
   );
 };
